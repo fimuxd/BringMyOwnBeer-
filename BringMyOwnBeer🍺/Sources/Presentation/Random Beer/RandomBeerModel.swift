e4c21b02a2086a42dcdc5cbe79a9d8ab8189b49e
Bo-Young PARK <fimuxd@gmail.com>
2019-06-14 16:36:33 +0900

    Initial commit for random beer view and view model

---
+++ BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerModel.swift
@@ -0,0 +1,9 @@
+//
+//  RandomBeerModel.swift
+//  BringMyOwnBeer🍺
+//
+//  Created by Boyoung Park on 14/06/2019.
+//  Copyright © 2019 Boyoung Park. All rights reserved.
+//
+
+import Foundation
---
+++ BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RandomBeerViewModel.swift
+//  BringMyOwnBeer🍺
+//
+//  Created by Boyoung Park on 14/06/2019.
+//  Copyright © 2019 Boyoung Park. All rights reserved.
+//
+
+import Foundation

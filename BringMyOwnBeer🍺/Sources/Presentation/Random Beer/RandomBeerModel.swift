8b247d2ca006fde99ae8b76b9effc024dfda6392
Bo-Young PARK <fimuxd@gmail.com>
2019-07-11 14:14:39 +0900

    Add random beer model

--- BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerModel.swift
+++ BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerModel.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019 Boyoung Park. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+struct RandomBeerModel {
+    let punkService: PunkService
+    
+    init(
+        punkService: PunkService = PunkServiceImpl(punkRepo: PunkRepositoryImpl())
+    ) {
+        self.punkService = punkService
+    }
+    
+    func getRandomBeer() -> Single<Result<[Beer], PunkServiceError>> {
+        return punkService.getRandomBeer()
+    }
+    
+    func parseData(value: [Beer]) -> BeerListCell.Data? {
+        guard let value = value.first else {
+            return nil
+        }
+        
+        return (id: value.id ?? 0, name: value.name ?? "", description: value.description ?? "", imageURL: value.imageURL ?? "")
+    }
+}

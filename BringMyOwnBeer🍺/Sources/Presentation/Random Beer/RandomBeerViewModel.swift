39b426213f2f37e434d64dc0e662b76741b4db91
Bo-Young PARK <fimuxd@gmail.com>
2019-07-11 14:14:34 +0900

    Add random beer view model

--- BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerViewModel.swift
+++ BringMyOwnBeer🍺/Sources/Presentation/Random Beer/RandomBeerViewModel.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2019 Boyoung Park. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+struct RandomBeerViewModel: RandomBeerViewBindable {
+    let randomButtonTapped = PublishRelay<Void>()
+    let selectedBeerData: Signal<BeerData>
+    let errorMessage: Signal<String>
+    
+    init(
+        model: RandomBeerModel = RandomBeerModel()
+    ) {
+        let beerResult = randomButtonTapped
+            .flatMapLatest(model.getRandomBeer)
+            .asObservable()
+            .share()
+        
+        let beerValue = beerResult
+            .map { result -> [Beer]? in
+                guard case .success(let value) = result else {
+                    return nil
+                }
+                return value
+            }
+            .filterNil()
+        
+        let beerError = beerResult
+            .map { result -> String? in
+                guard case .failure(let error) = result else {
+                    return nil
+                }
+                return error.message
+            }
+            .filterNil()
+        
+        self.selectedBeerData = beerValue
+            .map(model.parseData)
+            .filterNil()
+            .asSignal(onErrorSignalWith: .empty())
+        
+        self.errorMessage = beerError
+            .asSignal(onErrorJustReturn: "잠시 후 다시 시도해 주세요")
+    }
+}
